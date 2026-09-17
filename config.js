b547cd8b37f2ff7e28c7ce77dd0fabf41c9e3465
FreiwilligeFeuerwehrNeuenhagen <JulienEhr.JE@icloud.com>
2026-09-17 23:14:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://kyqcwbjocpcsrhkfgifn.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_H3gvVeen-UQ1YrIXaWdEsQ_hmGyYw-n"
 };
